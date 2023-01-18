b6194aa4ffdf25bfa0dac7f33ddcc16557a2ff8e
richardko1303 <help.covidrobot@gmail.com>
2023-01-18 21:38:36 +0100

    e

---
+++ plugins/teamgrid/project/updates/create_projects_table.php
@@ -0,0 +1,29 @@
+<?php namespace Teamgrid\Project\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('teamgrid_project_projects', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+
+            $table->string('title');
+            $table->integer('customer_id');
+            $table->integer('project_manager_id');
+
+            $table->timestamp('due_date')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('teamgrid_project_projects');
+    }
+}
